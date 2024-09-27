58ac0e835586662cce9b1f7c67711485cfc5c2ba
iceanddust <iceanddust@proton.me>
2024-09-27 19:51:19 -0300

    Start adding urn position overview section

--- src/modules/seal/components/SealDetails.tsx
+++ src/modules/seal/components/SealDetails.tsx
@@ -8,6 +8,10 @@ import { IntentMapping } from '@/lib/constants';
 import { useConfigContext } from '@/modules/config/hooks/useConfigContext';
 import { useUserSuggestedActions } from '@/modules/ui/hooks/useUserSuggestedActions';
 import { filterActionsByIntent } from '@/lib/utils';
+import { AboutSealModule } from '@/modules/ui/components/AboutSealModule';
+import { SealFaq } from './SealFaq';
+import { useState } from 'react';
+import { SealPositionOverview } from './SealPositionOverview';
 
 export function SealDetails(): React.ReactElement {
   const { isConnectedAndAcceptedTerms } = useConnectedContext();
@@ -15,20 +19,26 @@ export function SealDetails(): React.ReactElement {
   const { data: actionData } = useUserSuggestedActions();
   const widget = IntentMapping.SEAL_INTENT;
 
+  // TODO: Add logic to change selected position index based on widget, or URL params
+  const [selectedPositionIndex /*, setSelectedPositionIndex*/] = useState<number | null>(0);
+
   return (
     <DetailSectionWrapper>
-      <DetailSection title={t`Your position 01`}>
-        <DetailSectionRow>
-          {/* TODO: Add this section */}
-          <></>
-        </DetailSectionRow>
-      </DetailSection>
-      <DetailSection title={t`Position 01 details`}>
-        <DetailSectionRow>
-          {/* TODO: Add this section */}
-          <></>
-        </DetailSectionRow>
-      </DetailSection>
+      {selectedPositionIndex !== null && (
+        <>
+          <DetailSection title={t`Your position ${selectedPositionIndex}`}>
+            <DetailSectionRow>
+              <SealPositionOverview positionIndex={selectedPositionIndex} />
+            </DetailSectionRow>
+          </DetailSection>
+          <DetailSection title={t`Position ${selectedPositionIndex} details`}>
+            <DetailSectionRow>
+              {/* TODO: Add this section */}
+              <></>
+            </DetailSectionRow>
+          </DetailSection>
+        </>
+      )}
       {isConnectedAndAcceptedTerms &&
         !linkedActionConfig?.showLinkedAction &&
         (filterActionsByIntent(actionData?.linkedActions || [], widget).length ?? 0) > 0 && (
@@ -40,8 +50,7 @@ export function SealDetails(): React.ReactElement {
         )}
       <DetailSection title={t`About Seal module`}>
         <DetailSectionRow>
-          {/* TODO: Add this section */}
-          <></>
+          <AboutSealModule />
         </DetailSectionRow>
       </DetailSection>
       {isConnectedAndAcceptedTerms && (
@@ -60,8 +69,7 @@ export function SealDetails(): React.ReactElement {
       </DetailSection>
       <DetailSection title={t`FAQs`}>
         <DetailSectionRow>
-          {/* TODO: Add this section */}
-          <></>
+          <SealFaq />
         </DetailSectionRow>
       </DetailSection>
     </DetailSectionWrapper>
---
+++ src/modules/seal/components/SealPositionOverview.tsx
@@ -0,0 +1,13 @@
+// import { StatsCard } from '@/modules/ui/components/StatsCard';
+// import { t } from '@lingui/macro';
+
+export function SealPositionOverview({ positionIndex }: { positionIndex: number }): React.ReactElement {
+  console.log(positionIndex);
+  return (
+    <div>
+      {/* <StatsCard title={t`SKY sealed | MKR`} isLoading={} error={} content={} />
+      <StatsCard title={t`USDS borrowed`} isLoading={} error={} content={} />
+      <StatsCard title={t`Accumulated rewards`} isLoading={} error={} content={} /> */}
+    </div>
+  );
+}
