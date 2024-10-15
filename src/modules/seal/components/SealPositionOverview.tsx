e5721d53831d3203d18b3a64800d3caaf6591c45
bitsforpeace <bitsforpeace@proton.me>
2024-10-15 13:52:34 +0200

    Refactor reward and delegate to separate cards

--- src/modules/seal/components/SealPositionOverview.tsx
+++ src/modules/seal/components/SealPositionOverview.tsx
@@ -1,29 +1,20 @@
 import { HStack } from '@/modules/layout/components/HStack';
-import {
-  RiskLevel,
-  useDelegates,
-  useRewardContractTokens,
-  useSealPosition,
-  useUrnAddress,
-  useVault,
-  ZERO_ADDRESS
-} from '@jetstreamgg/hooks';
-import { formatAddress, formatBigInt, WAD_PRECISION } from '@jetstreamgg/utils';
+import { RiskLevel, useSealPosition, useUrnAddress, useVault, ZERO_ADDRESS } from '@jetstreamgg/hooks';
+import { formatBigInt, WAD_PRECISION } from '@jetstreamgg/utils';
 import { t, Trans } from '@lingui/macro';
 import { SealToken } from '../constants';
 import { SealPositionRewardsCard } from './SealPositionRewardsCard';
 import { SealBorrowedCard, SealSealedCard } from '@/modules/ui/components/BalanceCards';
 import { VStack } from '@/modules/layout/components/VStack';
 import { StatsCard } from '@/modules/ui/components/StatsCard';
-import { TokenIcon } from '@/modules/ui/components/TokenIcon';
 import { Heading, Text } from '@/modules/layout/components/Typography';
-import { useChainId } from 'wagmi';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { formatUnits } from 'viem';
 import { cn } from '@/lib/utils';
 import { DetailSection } from '@/modules/ui/components/DetailSection';
 import { DetailSectionRow } from '@/modules/ui/components/DetailSectionRow';
 import { formatUrnIndex } from './SealPositionDetails';
+import { SealDelegateCard } from './SealDelegateCard';
+import { SealRewardCard } from './SealRewardCard';
 
 const RISK_COLORS = {
   [RiskLevel.LIQUIDATION]: { text: 'text-red-400', bg: 'bg-red-400' },
@@ -37,24 +28,9 @@ export function SealPositionOverview({
 }: {
   positionIndex: number;
 }): React.ReactElement | null {
-  const chainId = useChainId();
   const { data, isLoading, error } = useSealPosition({ urnIndex: positionIndex });
-  const {
-    data: urnAddress,
-    isLoading: urnAddressLoading
-    // error: urnAddressError
-  } = useUrnAddress(BigInt(positionIndex));
+  const { data: urnAddress, isLoading: urnAddressLoading } = useUrnAddress(BigInt(positionIndex));
   const { data: vault, isLoading: vaultLoading, error: vaultError } = useVault(urnAddress || ZERO_ADDRESS);
-  const {
-    data: rewardContractTokens,
-    isLoading: tokensLoading,
-    error: tokensError
-  } = useRewardContractTokens(data?.selectedReward as `0x${string}` | undefined);
-  const {
-    data: delegates,
-    isLoading: delegatesLoading,
-    error: delegatesError
-  } = useDelegates({ chainId, pageSize: 1, search: data?.selectedDelegate });
 
   if (!error && !isLoading && !data) return null;
 
@@ -100,52 +76,8 @@ export function SealPositionOverview({
           </HStack>
           {(data?.selectedDelegate || data?.selectedReward) && (
             <HStack gap={2} className="w-full">
-              {data?.selectedReward && (
-                <StatsCard
-                  title={t`Reward`}
-                  isLoading={tokensLoading}
-                  error={tokensError}
-                  content={
-                    rewardContractTokens ? (
-                      <div className="mt-2 flex gap-2">
-                        <TokenIcon token={rewardContractTokens.rewardsToken} className="h-6 w-6" />
-                        <Text>{rewardContractTokens.rewardsToken.symbol}</Text>
-                      </div>
-                    ) : (
-                      <Text className="mt-2">{formatAddress(data.selectedReward, 6, 4)}</Text>
-                    )
-                  }
-                />
-              )}
-              {data?.selectedDelegate && (
-                <StatsCard
-                  title={t`Delegate`}
-                  isLoading={delegatesLoading}
-                  error={delegatesError}
-                  content={
-                    delegates ? (
-                      <div className="mt-2 flex gap-2">
-                        <Avatar className="h-6 w-6">
-                          <AvatarImage
-                            width={6}
-                            height={6}
-                            src={delegates[0].metadata?.image}
-                            alt={delegates[0].metadata?.name}
-                          />
-                          <AvatarFallback className="bg-slate-200 text-xs" delayMs={500}>
-                            {(delegates[0].metadata?.name.slice(0, 2) || 'SD').toUpperCase()}
-                          </AvatarFallback>
-                        </Avatar>
-                        <Text>
-                          {delegates[0].metadata?.name || formatAddress(data.selectedDelegate, 6, 4)}
-                        </Text>
-                      </div>
-                    ) : (
-                      <Text className="mt-2">{formatAddress(data.selectedDelegate, 6, 4)}</Text>
-                    )
-                  }
-                />
-              )}
+              {data?.selectedReward && <SealRewardCard selectedReward={data.selectedReward} />}
+              {data?.selectedDelegate && <SealDelegateCard selectedDelegate={data.selectedDelegate} />}
             </HStack>
           )}
           <HStack gap={2} className="scrollbar-thin w-full overflow-auto">
