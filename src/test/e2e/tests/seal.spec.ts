45d3dcb9f14fea5e5269cce79488e73b072af706
generatorpoint <generatorpoint@proton.me>
2024-10-17 15:35:22 -0600

    add seal steps

--- src/test/e2e/tests/seal.spec.ts
+++ src/test/e2e/tests/seal.spec.ts
@@ -1,16 +1,16 @@
 import { expect, test } from '@playwright/test';
 import '../mock-rpc-call.ts';
 import '../mock-vpn-check.ts';
-// import { approveOrPerformAction } from '../utils/approveOrPerformAction.ts';
-// import { setErc20Balance } from '../utils/setBalance.ts';
-// import { usdsAddress } from '@jetstreamgg/hooks';
-// import { TENDERLY_CHAIN_ID } from '@/data/wagmi/config/testTenderlyChain.ts';
+import { approveOrPerformAction } from '../utils/approveOrPerformAction.ts';
+import { setErc20Balance } from '../utils/setBalance.ts';
+import { mkrAddress } from '@jetstreamgg/hooks';
+import { TENDERLY_CHAIN_ID } from '@/data/wagmi/config/testTenderlyChain.ts';
 // import { withdrawAllAndReset } from '../utils/rewards.ts';
 import { connectMockWalletAndAcceptTerms } from '../utils/connectMockWalletAndAcceptTerms.ts';
 
-// test.beforeAll(async () => {
-//   await setErc20Balance(usdsAddress[TENDERLY_CHAIN_ID], '100');
-// });
+test.beforeAll(async () => {
+  await setErc20Balance(mkrAddress[TENDERLY_CHAIN_ID], '100');
+});
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
@@ -20,4 +20,35 @@ test.beforeEach(async ({ page }) => {
 
 test('initial seal module test', async ({ page }) => {
   await expect(page.getByText('About Seal Rewards')).toBeVisible();
+  await page.getByRole('checkbox').click();
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await expect(page.getByTestId('supply-first-input-lse-balance')).toHaveText('100 MKR');
+
+  // fill inputs and click next
+  await page.getByTestId('supply-first-input-lse').fill('100');
+  // TODO: check all the params
+  await expect(page.getByTestId('widget-button')).toBeEnabled();
+  await page.getByTestId('widget-button').click();
+
+  // select rewards
+  await expect(page.getByText('Select token rewards')).toBeVisible();
+  await page.getByRole('button', { name: 'USDS' }).click();
+  await expect(page.getByTestId('widget-button')).toBeEnabled();
+  await page.getByTestId('widget-button').click();
+
+  // select delegate
+  await expect(page.getByText('Select delegate')).toBeVisible();
+  await page.getByRole('button', { name: '0x278' }).click();
+  await expect(page.getByTestId('widget-button')).toBeEnabled();
+  await page.getByTestId('widget-button').click();
+
+  // confirm position
+  await expect(page.getByText('Confirm position')).toBeVisible();
+  await expect(page.getByText('Sealing')).toBeVisible();
+  await expect(page.getByText('100 MKR')).toBeVisible();
+  await expect(page.getByText('Seal rewards')).toBeVisible();
+
+  await approveOrPerformAction(page, 'Approve seal amount');
+  expect(page.getByRole('heading', { name: 'Waiting for confirmation' })).toBeVisible();
+  await approveOrPerformAction(page, 'Open position');
 });
--- src/test/e2e/utils/approveOrPerformAction.ts
+++ src/test/e2e/utils/approveOrPerformAction.ts
@@ -8,7 +8,9 @@ export type Action =
   | 'Revert'
   | 'Trade'
   | 'Approve'
-  | 'Approve supply amount';
+  | 'Approve supply amount'
+  | 'Approve seal amount'
+  | 'Open position';
 
 type approveOrPerformActionOptions = {
   reject?: boolean;
