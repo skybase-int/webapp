50060c70a7113c537a240f91804fa2b6e3a9a9d9
iceanddust <iceanddust@proton.me>
2024-10-18 13:56:15 -0300

    Revert import change to fix build

--- src/data/wagmi/config/testTenderlyChain.ts
+++ src/data/wagmi/config/testTenderlyChain.ts
@@ -1,5 +1,5 @@
 import { defineChain } from 'viem';
-import { readFileSync } from 'fs';
+import tenderlyTestnetData from '../../../../tenderlyTestnetData.json' assert { type: 'json' };
 
 export const TENDERLY_CHAIN_ID = 314310;
 // only works if hardcoded, cannot be set via env variable. Corresponds to the public RPC of `mainnet_sep_30_0`
@@ -7,9 +7,7 @@ export const TENDERLY_RPC_URL =
   'https://virtual.mainnet.rpc.tenderly.co/b333d3ac-c24f-41fa-ad41-9176fa719ac3';
 
 export const getTestTenderlyChain = () => {
-  const tenderlyTestnetData = readFileSync('../../../../tenderlyTestnetData.json', 'utf8');
-  const parsedData = JSON.parse(tenderlyTestnetData);
-  const { TENDERLY_RPC_URL: TEMP_RPC_URL } = parsedData;
+  const { TENDERLY_RPC_URL: TEMP_RPC_URL } = tenderlyTestnetData;
   const rpc = TEMP_RPC_URL || TENDERLY_RPC_URL;
   return defineChain({
     id: TENDERLY_CHAIN_ID,
