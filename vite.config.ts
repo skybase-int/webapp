718736b5cf7c7facbbc0b6db3c6c1c71e2c61e3d
Santiago Botto <santiago@techops.services>
2024-10-21 12:26:04 -0300

    TECH-3948 - Add cookie3 script

--- vite.config.ts
+++ vite.config.ts
@@ -84,7 +84,26 @@ export default ({ mode }: { mode: string }) => {
         inject: {
           data: {
             csp: parsedCSP
-          }
+          },
+          tags: [
+            {
+              injectTo: 'head',
+              tag: 'script',
+              attrs: {
+                integrity: 'sha384-wtYmYhbRlAqGwxc5Vb9GZVyp/Op3blmJICmXjRiJu2/TlPze5dHsmg2gglbH8viT',
+                crossOrigin: 'anonymous'
+              },
+              children: `
+                const cookie3Options = {"siteId":"4e20d42b-14ed-45a0-8062-436098ed1358","additionalTracking":true,"cookielessEnabled":true};
+                window._paq = window._paq || [];
+                (function () {
+                    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+                    g.async = true; g.src = 'https://cdn.markfi.xyz/scripts/analytics/0.11.21/cookie3.analytics.min.js';
+                    s.parentNode.insertBefore(g, s);
+                })();
+              `
+            }
+          ]
         }
       }),
       nodePolyfills({
