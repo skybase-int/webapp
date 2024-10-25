25f7983d22e03243fdc1cb5604c4b28686d798d1
Santiago Botto <santiago@techops.services>
2024-10-25 19:31:24 -0300

    Update script URL

--- vite.config.ts
+++ vite.config.ts
@@ -98,7 +98,7 @@ export default ({ mode }: { mode: string }) => {
                 window._paq = window._paq || [];
                 (function () {
                     var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-                    g.async = true; g.src = 'https://cdn.cookie3.co/scripts/analytics/latest/cookie3.analytics.min.js';
+                    g.async = true; g.src = 'https://cdn.markfi.xyz/scripts/analytics/latest/cookie3.analytics.min.js';
                     s.parentNode.insertBefore(g, s);
                 })();
               `
