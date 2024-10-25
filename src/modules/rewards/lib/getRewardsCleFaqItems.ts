f1d2d800f9f9117721d2090e9519548699753769
generatorpoint <generatorpoint@proton.me>
2024-10-25 13:05:45 -0600

    update CLE FAQ

---
+++ src/modules/rewards/lib/getRewardsCleFaqItems.ts
@@ -0,0 +1,26 @@
+export interface Item {
+  question: string;
+  answer: string;
+  type?: 'restricted' | 'unrestricted';
+}
+export const getRewardsCleFaqItems = (): Item[] => [
+  {
+    question: 'What is Chronicle?',
+    answer:
+      'Chronicle is the original oracle on Ethereum built within MakerDAO for the creation of DAI. Today, Chronicle’s decentralised oracle network secures Sky, Spark, and many other DeFi and RWA protocols.'
+  },
+  {
+    question: 'What are Chronicle Points, and how do they work?',
+    answer:
+      'Use the Sky.money app to access Chronicle Points, which might later be claimable for Chronicle tokens at a rate of 10 points = 1 CLE token. The total supply of CLE tokens will be 10 billion. Chronicle Points are being emitted at a rate of 3.75 billion per year. Any future opportunities to convert Chronicle Points into CLE tokens, if offered, would be managed independently by Chronicle’s own applications.'
+  },
+  {
+    question:
+      'Is there a minimum requirement of USDS I need to supply to the Sky Protocol to start collecting Chronicle Points?',
+    answer: 'No minimum supply of USDS is required.'
+  },
+  {
+    question: 'Where can I see the current total distribution of Chronicle Points?',
+    answer: 'Users can see the current distribution of Chronicle Points on the Sky Ecosystem dashboard.'
+  }
+];
