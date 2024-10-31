4fca8d2a59ab9c84d648305836da0fbb260dae93
generatorpoint <generatorpoint@proton.me>
2024-10-31 15:23:37 -0600

    WIP manage position test

--- src/test/e2e/tests/seal.spec.ts
+++ src/test/e2e/tests/seal.spec.ts
@@ -24,8 +24,10 @@ test('Lock MKR, select rewards, select delegate, and open position', async ({ pa
   await page.getByRole('button', { name: 'Continue' }).click();
   await expect(page.getByTestId('supply-first-input-lse-balance')).toHaveText('100 MKR');
 
-  // fill inputs and click next
+  // fill seal and borrow inputs and click next
   await page.getByTestId('supply-first-input-lse').fill('100');
+  await page.getByTestId('borrow-input-lse').fill('38000');
+
   // TODO: check all the params
   await expect(page.getByTestId('widget-button')).toBeEnabled();
   await page.getByTestId('widget-button').click();
@@ -46,13 +48,25 @@ test('Lock MKR, select rewards, select delegate, and open position', async ({ pa
   await expect(page.getByText('Confirm your position').nth(0)).toBeVisible();
   await expect(page.getByTestId('widget-container').getByText('Sealing')).toBeVisible();
   await expect(page.getByText('100 MKR')).toBeVisible();
+  await expect(page.getByTestId('widget-container').getByText('Borrowing')).toBeVisible();
+  await expect(page.getByText('38,000 USDS')).toBeVisible();
   await expect(page.getByTestId('widget-container').getByText('Seal reward')).toBeVisible();
 
   await approveOrPerformAction(page, 'Approve seal amount');
   expect(page.getByRole('heading', { name: 'Token access approved' })).toBeVisible();
   await approveOrPerformAction(page, 'Continue');
   expect(page.getByRole('heading', { name: 'Success!' })).toBeVisible();
-  await expect(page.getByText("You've sealed 100 MKR. Your new position is open.")).toBeVisible();
+  // await expect(page.getByText("You've sealed 100 MKR. Your new position is open.")).toBeVisible();
   await page.getByRole('button', { name: 'Manage your position(s)' }).click();
   await expect(page.getByText('Position 1')).toBeVisible();
+
+  await page.getByRole('button', { name: 'Manage Position' }).click();
+
+  await expect(page.getByText('Your position 1')).toBeVisible();
+  // await expect(page.getByTestId('borrow-input-lse-balance')).toHaveText('Limit 0 <> 17,593 USDS');
+
+  await page.getByTestId('borrow-input-lse').fill('100');
+
+  // TODO: Insufficient balance error appears, but it shouldn't
+  await expect(page.getByText('Insufficient collateral')).not.toBeVisible();
 });
