72fe076657355c02dd508119f6184b39b4d531f1
generatorpoint <generatorpoint@proton.me>
2024-11-26 14:10:07 -0600

    enable SKY in upgrade

--- src/modules/upgrade/components/UpgradeAbout.tsx
+++ src/modules/upgrade/components/UpgradeAbout.tsx
@@ -2,12 +2,10 @@ import { AboutSky } from '@/modules/ui/components/AboutSky';
 import { AboutUsds } from '@/modules/ui/components/AboutUsds';
 
 export function UpgradeAbout() {
-  const isRestricted = import.meta.env.VITE_RESTRICTED_BUILD === 'true';
-
   return (
     <div>
       <AboutUsds />
-      {!isRestricted && <AboutSky />}
+      <AboutSky />
     </div>
   );
 }
