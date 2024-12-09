a37ec498913d868f11f8a7d7d3a98c028ce400bf
generatorpoint <generatorpoint@proton.me>
2024-12-09 16:35:30 -0700

    update TokenIcon and add base logos

--- src/modules/ui/components/TokenIcon.tsx
+++ src/modules/ui/components/TokenIcon.tsx
@@ -1,7 +1,9 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { cn } from '@/lib/utils';
 import { Token } from '@jetstreamgg/hooks';
+import { isBaseChainId } from '@jetstreamgg/utils';
 import React from 'react';
+import { useChainId } from 'wagmi';
 
 export function TokenIcon({
   token,
@@ -14,16 +16,14 @@ export function TokenIcon({
   className?: string;
   fallbackClassName?: string;
 }): React.ReactElement {
+  const chainId = useChainId();
   if (!token.symbol) return <></>;
+
+  const path = `/tokens/${isBaseChainId(chainId) ? 'base/' : ''}${token.symbol.toLowerCase()}.svg`;
 
   return (
     <Avatar className={cn('', className)}>
-      <AvatarImage
-        width={width}
-        height={width}
-        src={`/tokens/${token.symbol.toLowerCase()}.svg`}
-        alt={token.name}
-      />
+      <AvatarImage width={width} height={width} src={path} alt={token.name} />
       <AvatarFallback className={cn('bg-slate-200 text-xs', fallbackClassName)} delayMs={500}>
         {token.symbol.toUpperCase()}
       </AvatarFallback>
