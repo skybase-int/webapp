283f306cbb057327a16b62acf932c8d6ebed5652
Code with the Flow <codewiththeflowxyz@proton.me>
2025-01-06 12:51:54 +0100

    Update default config

--- src/modules/config/default-config.ts
+++ src/modules/config/default-config.ts
@@ -2,9 +2,14 @@
 
 import { defaultConfig as widgetsConfig } from '@jetstreamgg/widgets';
 import { SiteConfig } from './types/site-config';
-import { restrictedTokenList, restrictedTokenListTrade } from './restrictedTokenList';
+import {
+  restrictedTokenList,
+  restrictedTokenListMiCa,
+  restrictedTokenListTrade
+} from './restrictedTokenList';
 
 const restrictedBuild = import.meta.env.VITE_RESTRICTED_BUILD === 'true';
+const restrictedMiCa = import.meta.env.VITE_RESTRICTED_BUILD_MICA === 'true';
 
 // stores all the RPCs the application will use, and also the user configured-ones
 export const defaultConfig: SiteConfig = {
@@ -19,5 +24,6 @@ export const defaultConfig: SiteConfig = {
         tradeTokenList: restrictedTokenListTrade,
         balancesTokenList: restrictedTokenList
       }
-    : {})
+    : {}),
+  ...(restrictedMiCa ? { balancesTokenList: restrictedTokenListMiCa } : {})
 };
