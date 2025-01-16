67c24ab32c9c57406347ad22519bd44b05645bd3
Code with the Flow <codewiththeflowxyz@proton.me>
2025-01-16 12:58:50 +0100

    Add new style to not available ModuleCards

--- src/modules/balances/components/ModuleCard.tsx
+++ src/modules/balances/components/ModuleCard.tsx
@@ -9,6 +9,8 @@ import { Card, CardTitle, CardContent } from '@/components/ui/card';
 import { Heading, Text } from '@/modules/layout/components/Typography';
 import { useRetainedQueryParams } from '@/modules/ui/hooks/useRetainedQueryParams';
 import { ChainModal } from '@/modules/ui/components/ChainModal';
+import { isBaseChainId } from '@jetstreamgg/utils';
+import { useChainId } from 'wagmi';
 
 type ModuleCardProps = {
   className: string;
@@ -20,6 +22,8 @@ type ModuleCardProps = {
 };
 
 export const ModuleCard = ({ className, title, intent, module, notAvailable, soon }: ModuleCardProps) => {
+  const chainId = useChainId();
+  const isBase = isBaseChainId(chainId);
   const url = useRetainedQueryParams(`/?widget=${mapIntentToQueryParam(intent)}`);
   const content = (
     <>
@@ -31,16 +35,7 @@ export const ModuleCard = ({ className, title, intent, module, notAvailable, soo
           <Trans>Soon on Base</Trans>
         </Text>
       )}
-      <Card
-        className={cn(
-          `relative flex h-full flex-col justify-between bg-[length:100%_100%] ${
-            notAvailable
-              ? 'before:absolute before:inset-0 before:rounded-[20px] before:bg-black before:opacity-50'
-              : ''
-          }`,
-          className
-        )}
-      >
+      <Card className={cn('relative flex h-full flex-col justify-between bg-[length:100%_100%]', className)}>
         <CardTitle
           className={`mb-7 text-left font-custom-450 font-normal leading-8 ${notAvailable ? '' : ''}`}
         >
@@ -49,7 +44,7 @@ export const ModuleCard = ({ className, title, intent, module, notAvailable, soo
         <CardContent className="relative p-0 pb-2">
           <HStack className="items-center justify-between">
             <Heading variant="extraSmall" className="text-left">
-              {notAvailable ? t`View on Mainnet` : t`Go to ${module}`}
+              {notAvailable ? t`View on Mainnet` : t`Go to ${isBase ? 'Base' : ''} ${module}`}
             </Heading>
             <ArrowRight />
           </HStack>
